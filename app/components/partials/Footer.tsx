b20d4e369a08be1f7914b3c903a6de8a167eb6c4
Mark Roland Alueta <markintoshroland@gmail.com>
2024-05-03 19:02:36 +0800

    Refactor styling on Footer

--- app/components/partials/Footer.tsx
+++ app/components/partials/Footer.tsx
@@ -4,8 +4,8 @@ type FooterProps = HTMLAttributes<HTMLElement>;
 
 export default function Footer({ ...props }: FooterProps) {
   return (
-    <footer className="p-2 w-full text-xs lg:text-sm" {...props}>
-      <ul className="flex flex-row items-center w-full justify-center gap-5">
+    <footer className="p-2 w-full text-xs lg:text-sm mt-5 lg:mt-0" {...props}>
+      <ul className="flex flex-row items-center w-full justify-center gap-2 lg:gap-5 text-center">
         <li>
           <a href="#">Privacy</a>
         </li>
