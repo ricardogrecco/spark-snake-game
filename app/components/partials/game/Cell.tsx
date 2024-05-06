3e5450b2163bfa5f5d9c28695ecd035f507b1443
Mark Roland Alueta <markintoshroland@gmail.com>
2024-05-06 13:19:57 +0800

    Refactor Cell component and Updated SVGS usage

--- app/components/partials/game/Cell.tsx
+++ app/components/partials/game/Cell.tsx
@@ -2,10 +2,11 @@
 import { SnakeDirection } from "@/app/types";
 
 // SVGS
-import SNAKE_HEAD_OPEN from "assets/SnakeHeadOpen.svg";
-import SNAKE_HEAD from "assets/SnakeHead.svg";
-import SNAKE_BODY from "assets/SnakeBody.svg";
-import FRUIT from "assets/Fruit.svg";
+import SNAKE_HEAD_OPEN from "@/public/assets/SnakeHeadOpen.svg";
+import SNAKE_HEAD from "@/public/assets/SnakeHead.svg";
+import SNAKE_BODY from "@/public/assets/SnakeBody.svg";
+import FRUIT from "@/public/assets/Fruit.svg";
+import React from "react";
 
 type CellProps = {
   isSnakeHead: boolean;
@@ -16,41 +17,64 @@ type CellProps = {
   checkered?: boolean;
 };
 
-export default function Cell({
-  isSnakeHead,
-  isSnakeBody,
-  direction,
-  isFruit,
-  isNearFruit,
-  checkered = false,
-}: CellProps) {
-  return (
-    <div
-      className={`w-5 h-5 flex items-center justify-center ${
-        checkered && "bg-base-300"
-      }`}
-    >
-      {isSnakeHead && (
-        <img
-          src={
-            isNearFruit ? "assets/SnakeHeadOpen.svg" : "assets/SnakeHead.svg"
-          }
-          alt="Snake Head"
-          className={
-            direction === "UP"
-              ? "transform -rotate-90"
-              : direction === "DOWN"
-              ? "transform rotate-90"
-              : direction === "LEFT"
-              ? "transform rotate-180"
-              : ""
-          }
-        />
-      )}
-      {isSnakeBody && (
-        <img src="assets/SnakeBody.svg" alt="Snake Body" className="w-4 h-4" />
-      )}
-      {isFruit && <img src="assets/Fruit.svg" alt="Fruit" />}
-    </div>
-  );
-}
+const Cell = React.memo(
+  ({
+    isSnakeHead,
+    isSnakeBody,
+    direction,
+    isFruit,
+    isNearFruit,
+    checkered = false,
+  }: CellProps) => {
+    return (
+      <div
+        className={`w-5 h-5 flex items-center justify-center ${
+          checkered && "bg-base-300"
+        }`}
+      >
+        {isSnakeHead && isNearFruit && (
+          <SNAKE_HEAD_OPEN
+            alt="Snake Head Open"
+            className={`w-5 h-5 absolute 
+              ${
+                direction === "UP"
+                  ? "transform -rotate-90"
+                  : direction === "DOWN"
+                  ? "transform rotate-90"
+                  : direction === "LEFT"
+                  ? "transform rotate-180"
+                  : ""
+              }
+            `}
+          />
+        )}
+        {isSnakeHead && !isNearFruit && (
+          <SNAKE_HEAD
+            alt="Snake Head"
+            className={`w-5 h-5 absolute 
+              ${
+                direction === "UP"
+                  ? "transform -rotate-90"
+                  : direction === "DOWN"
+                  ? "transform rotate-90"
+                  : direction === "LEFT"
+                  ? "transform rotate-180"
+                  : ""
+              }
+            `}
+          />
+        )}
+
+        {isSnakeBody && (
+          <SNAKE_BODY alt="Snake Body" className="absolute w-4 h-4" />
+        )}
+
+        {isFruit && <FRUIT alt="Fruit" className="absolute w-5 h-5" />}
+      </div>
+    );
+  }
+);
+
+Cell.displayName = "Cell";
+
+export default Cell;
