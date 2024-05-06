d41de497655c0977f68d60e9cdf1dec7f9626682
Mark Roland Alueta <markintoshroland@gmail.com>
2024-05-06 11:44:49 +0800

    Added isNearFruit helper function

---
+++ app/helpers/isSnakeNearFruit.ts
@@ -0,0 +1,8 @@
+import { FruitProps, SnakeProps } from "../types";
+
+export const isSnakeNearFruit = (snake: SnakeProps, fruit: FruitProps) => {
+  const diffX = Math.abs(snake.x - fruit.x);
+  const diffY = Math.abs(snake.y - fruit.y);
+
+  return diffX <= 1 && diffY <= 1;
+};
