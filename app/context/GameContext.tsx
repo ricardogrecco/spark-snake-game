eadc159b42c74f7f45342c60c9f31a2e769c3574
Mark Roland Alueta <markintoshroland@gmail.com>
2024-05-07 23:52:35 +0800

    Added Timer functionality

--- app/context/GameContext.tsx
+++ app/context/GameContext.tsx
@@ -2,8 +2,16 @@
 
 import { createContext, useEffect, useRef, useState } from "react";
 import { FruitProps, SnakeDirection, SnakeProps } from "../types";
-import { BOARD_SIZE } from "../utils/constants";
+import {
+  BOARD_SIZE,
+  SNAKE_SPEED,
+  SNAKE_SPEED_INCREMENT,
+  TIMER,
+} from "../utils/constants";
 import { generateFruit } from "../helpers/generateFruit";
+import { useKeyboardDirectionEffect } from "../hooks/KeyboardDirectionEffect";
+import useMoveSnakeCallback from "../hooks/MoveSnakeCallback";
+import { useSnakeIntervalEffect } from "../hooks/SnakeIntervalEffect";
 
 type GameContextType = {
   direction: SnakeDirection;
@@ -19,6 +27,9 @@ type GameContextType = {
   gameOver: boolean;
   setGameOver: React.Dispatch<React.SetStateAction<boolean>>;
   snakeInterval: React.MutableRefObject<NodeJS.Timeout | null>;
+  timer: number;
+  setTimer: React.Dispatch<React.SetStateAction<number>>;
+  timerInterval: React.MutableRefObject<NodeJS.Timeout | null>;
 };
 
 export const GameContext = createContext<GameContextType>({
@@ -35,6 +46,9 @@ export const GameContext = createContext<GameContextType>({
   gameOver: false,
   setGameOver: () => {},
   snakeInterval: { current: null },
+  timer: 1000 * 60,
+  setTimer: () => {},
+  timerInterval: { current: null },
 });
 
 export default function GameProvider({
@@ -56,11 +70,17 @@ export default function GameProvider({
   const [score, setScore] = useState<number>(0);
   const [gameOver, setGameOver] = useState<boolean>(false);
 
+  const [timer, setTimer] = useState<number>(1000 * TIMER);
+  const timerInterval = useRef<NodeJS.Timeout | null>(null);
+
+  const moveSnake = useMoveSnakeCallback();
+
   useEffect(() => {
-    if (!gameOver) {
-      if (snakeInterval.current) {
-        clearInterval(snakeInterval.current);
-      }
+    if (gameOver) {
+      if (timerInterval.current) clearInterval(timerInterval.current);
+      if (snakeInterval.current) clearInterval(snakeInterval.current);
+    } else {
+      if (snakeInterval.current) clearInterval(snakeInterval.current);
 
       setSnake([
         {
@@ -76,10 +96,29 @@ export default function GameProvider({
 
       setScore(0);
 
-      const interval = setInterval(() => {}, 100);
+      snakeInterval.current = setInterval(
+        moveSnake,
+        SNAKE_SPEED - Math.sqrt(tailLength) * SNAKE_SPEED_INCREMENT
+      );
 
-      snakeInterval.current = interval;
+      setTimer(1000 * TIMER);
+      timerInterval.current = setInterval(() => {
+        setTimer((prevTimer) => {
+          if (prevTimer <= 0) {
+            clearInterval(timerInterval.current!);
+            setGameOver(true);
+            return 0;
+          }
+
+          return prevTimer - 1000;
+        });
+      }, 1000);
     }
+
+    return () => {
+      if (timerInterval.current) clearInterval(timerInterval.current);
+      if (snakeInterval.current) clearInterval(snakeInterval.current);
+    };
   }, [gameOver]);
 
   return (
@@ -106,6 +145,10 @@ export default function GameProvider({
           setGameOver,
           // Intervals
           snakeInterval,
+          // timer
+          timer,
+          setTimer,
+          timerInterval,
         } as GameContextType
       }
     >
