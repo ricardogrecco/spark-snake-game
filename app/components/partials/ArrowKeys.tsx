bcbe8c2924cacdbcd7bee338a9c1b19f06d38ca5
Mark Roland Alueta <markintoshroland@gmail.com>
2024-05-08 22:29:18 +0800

    Added ArrowKeys buttons logic

--- app/components/partials/ArrowKeys.tsx
+++ app/components/partials/ArrowKeys.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   BiSolidDownArrow,
   BiSolidLeftArrow,
@@ -5,28 +7,50 @@ import {
   BiSolidUpArrow,
 } from "react-icons/bi";
 
-export default function ArrowKeys() {
+type ArrowKeysProps = {
+  disabled?: boolean;
+};
+
+export default function ArrowKeys({ disabled = true }: ArrowKeysProps) {
+  const dispatchArrowKey = (key: string) => {
+    if (disabled) return;
+    const event = new KeyboardEvent("keydown", { key });
+    window.dispatchEvent(event);
+  };
+
   return (
     <section className="w-48 h-48 lg:w-48 lg:h-48 self-center -mb-14">
       <div className="grid grid-cols-3 grid-rows-3 h-full w-full">
         <div className="w-full h-full"></div>
         <div className="bg-[#772BCB] w-full h-full p-2 rounded-t">
-          <button className="bg-[#430E7D] btn w-full h-full p-3 items-center flex justify-center rounded shadow-md">
+          <button
+            onClick={() => dispatchArrowKey("ArrowUp")}
+            className="bg-[#430E7D] btn w-full h-full p-3 items-center flex justify-center rounded shadow-md"
+          >
             <BiSolidUpArrow className="w-full h-full" />
           </button>
         </div>
         <div className="w-full h-full"></div>
-        <div className="bg-[#772BCB] w-full h-full p-2 rounded-l">
+        <div
+          onClick={() => dispatchArrowKey("ArrowLeft")}
+          className="bg-[#772BCB] w-full h-full p-2 rounded-l"
+        >
           <button className="bg-[#430E7D] btn w-full h-full p-3 items-center flex justify-center rounded shadow-md">
             <BiSolidLeftArrow className="w-full h-full" />
           </button>
         </div>
-        <div className="bg-[#772BCB] w-full h-full p-2">
+        <div
+          onClick={() => dispatchArrowKey("ArrowDown")}
+          className="bg-[#772BCB] w-full h-full p-2"
+        >
           <button className="bg-[#430E7D] btn w-full h-full p-3 items-center flex justify-center shadow-md">
             <BiSolidDownArrow className="w-full h-full" />
           </button>
         </div>
-        <div className="bg-[#772BCB] w-full h-full p-2 rounded-r">
+        <div
+          onClick={() => dispatchArrowKey("ArrowRight")}
+          className="bg-[#772BCB] w-full h-full p-2 rounded-r"
+        >
           <button className="bg-[#430E7D] btn w-full h-full p-3 items-center flex justify-center rounded shadow-md">
             <BiSolidRightArrow className="w-full h-full" />
           </button>
