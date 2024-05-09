e218f010611c7aeca1b4c3d31438e28e3a93242a
Mark Roland Alueta <markintoshroland@gmail.com>
2024-05-09 22:23:02 +0800

    Update ArrowKeys design

--- app/components/partials/ArrowKeys.tsx
+++ app/components/partials/ArrowKeys.tsx
@@ -20,7 +20,7 @@ export default function ArrowKeys({ disabled = true }: ArrowKeysProps) {
 
   return (
     <section className="w-48 h-48 lg:w-48 lg:h-48 self-center -mb-14">
-      <div className="grid grid-cols-3 grid-rows-3 h-full w-full">
+      <div className="grid grid-cols-3 grid-rows-3 h-full w-full justify-items-center">
         <div className="w-full h-full"></div>
         <div className="bg-[#772BCB] w-full h-full p-2 rounded-t">
           <button
@@ -31,10 +31,10 @@ export default function ArrowKeys({ disabled = true }: ArrowKeysProps) {
           </button>
         </div>
         <div className="w-full h-full"></div>
-        <div className="bg-[#772BCB] w-full h-full p-2 rounded-l">
+        <div className="bg-[#772BCB] w-max h-full pl-2 py-2 rounded-l ml-2">
           <button
             onClick={() => dispatchArrowKey("ArrowLeft")}
-            className="bg-[#430E7D] btn w-full h-full p-3 items-center flex justify-center rounded shadow-md"
+            className="bg-[#430E7D] btn w-22 h-full p-3 items-center flex justify-center rounded shadow-md"
           >
             <BiSolidLeftArrow className="w-full h-full" />
           </button>
@@ -47,10 +47,10 @@ export default function ArrowKeys({ disabled = true }: ArrowKeysProps) {
             <BiSolidDownArrow className="w-full h-full" />
           </button>
         </div>
-        <div className="bg-[#772BCB] w-full h-full p-2 rounded-r">
+        <div className="bg-[#772BCB] w-max h-full pr-2 py-2 rounded-r mr-2">
           <button
             onClick={() => dispatchArrowKey("ArrowRight")}
-            className="bg-[#430E7D] btn w-full h-full p-3 items-center flex justify-center rounded shadow-md"
+            className="bg-[#430E7D] btn w-22 h-full p-3 items-center flex justify-center rounded shadow-md"
           >
             <BiSolidRightArrow className="w-full h-full" />
           </button>
