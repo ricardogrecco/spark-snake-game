b7f28ddd6425c02d98efe817893a1773fefc587a
Mark Roland Alueta <markintoshroland@gmail.com>
2024-05-20 19:15:38 +0800

    Added Side Menu background image

--- app/components/partials/Drawer.tsx
+++ app/components/partials/Drawer.tsx
@@ -19,6 +19,11 @@ export default function Drawer({ children }: { children: React.ReactNode }) {
           className="drawer-overlay"
         ></label>
         <div className="w-max bg-base-100 h-full p-10 flex flex-col gap-5">
+          <img
+            src="/assets/bg/sidemenu-bg.webp"
+            alt="Side Menu Background"
+            className="absolute inset-0 w-full h-full object-cover -z-10 mix-blend-luminosity"
+          />
           <div className="w-full flex-grow flex justify-end">
             <label
               className="btn btn-circle bg-[#772bcb]"
