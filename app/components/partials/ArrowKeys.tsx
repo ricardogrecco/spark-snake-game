b0b9e92e372c959899a78e08a9d6913c284a5737
Mark Roland Alueta <markintoshroland@gmail.com>
2024-05-20 19:55:50 +0800

    Changed styling on Arrow Keys

--- app/components/partials/ArrowKeys.tsx
+++ app/components/partials/ArrowKeys.tsx
@@ -25,7 +25,7 @@ export default function ArrowKeys({ disabled = true }: ArrowKeysProps) {
         <div className="bg-[#772BCB] w-full h-full pt-2 px-2 rounded-t mt-2">
           <button
             onClick={() => dispatchArrowKey("ArrowUp")}
-            className="bg-[#430E7D] btn w-22 h-3 p-3 items-center flex justify-center rounded shadow-md"
+            className="bg-[#430E7D] btn w-22 h-3 p-3 items-center flex justify-center rounded ring-neon ring-1 ring-offset-neon ring-offset-1 shadow-neon relative"
           >
             <BiSolidUpArrow className="w-full h-full" />
           </button>
@@ -34,7 +34,7 @@ export default function ArrowKeys({ disabled = true }: ArrowKeysProps) {
         <div className="bg-[#772BCB] w-max h-full pl-2 py-2 rounded-l ml-2">
           <button
             onClick={() => dispatchArrowKey("ArrowLeft")}
-            className="bg-[#430E7D] btn w-22 h-full p-3 items-center flex justify-center rounded shadow-md"
+            className="bg-[#430E7D] btn w-22 h-full p-3 items-center flex justify-center rounded ring-neon ring-1 ring-offset-neon ring-offset-1 shadow-neon relative"
           >
             <BiSolidLeftArrow className="w-full h-full" />
           </button>
@@ -42,7 +42,7 @@ export default function ArrowKeys({ disabled = true }: ArrowKeysProps) {
         <div className="bg-[#772BCB] w-full h-full p-2">
           <button
             onClick={() => dispatchArrowKey("ArrowDown")}
-            className="bg-[#430E7D] btn w-full h-full p-3 items-center flex justify-center rounded shadow-md"
+            className="bg-[#430E7D] btn w-full h-full p-3 items-center flex justify-center rounded ring-neon ring-1 ring-offset-neon ring-offset-1 shadow-neon relative"
           >
             <BiSolidDownArrow className="w-full h-full" />
           </button>
@@ -50,7 +50,7 @@ export default function ArrowKeys({ disabled = true }: ArrowKeysProps) {
         <div className="bg-[#772BCB] w-max h-full pr-2 py-2 rounded-r mr-2">
           <button
             onClick={() => dispatchArrowKey("ArrowRight")}
-            className="bg-[#430E7D] btn w-22 h-full p-3 items-center flex justify-center rounded shadow-md"
+            className="bg-[#430E7D] btn w-22 h-full p-3 items-center flex justify-center rounded ring-neon ring-1 ring-offset-neon ring-offset-1 shadow-neon relative"
           >
             <BiSolidRightArrow className="w-full h-full" />
           </button>
