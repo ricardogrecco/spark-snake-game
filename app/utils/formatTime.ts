972c2211675b503e713830ee53ed3b4dd31f7524
Mark Roland Alueta <markintoshroland@gmail.com>
2024-05-23 10:50:09 +0800

    Refactor formatTime to have "00:00" format

--- app/utils/formatTime.ts
+++ app/utils/formatTime.ts
@@ -1,7 +1,9 @@
 export const formatTime = (milliseconds: number): string => {
   const totalSeconds = Math.floor(milliseconds / 1000);
-  const minutes = Math.floor(totalSeconds / 60);
-  const seconds = totalSeconds % 60;
+  const minutes = Math.floor(totalSeconds / 60)
+    .toString()
+    .padStart(2, "0");
+  const seconds = (totalSeconds % 60).toString().padStart(2, "0");
 
-  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  return `${minutes}:${seconds}`;
 };
