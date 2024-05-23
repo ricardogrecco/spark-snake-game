b4f70f725224ab7d9b37d87af9ec81f29a15375c
Mark Roland Alueta <markintoshroland@gmail.com>
2024-05-23 11:17:00 +0800

    Update styling on ArrowKey pads and buttons

--- app/components/partials/ArrowKeys.tsx
+++ app/components/partials/ArrowKeys.tsx
@@ -26,10 +26,10 @@ export default function ArrowKeys({
     <section className="w-48 h-48 lg:w-48 lg:h-48 self-center -mb-14">
       <div className="grid grid-cols-3 grid-rows-3 h-full w-full justify-items-center">
         <div className="w-full h-full"></div>
-        <div className="bg-[#772BCB] w-full h-full pt-2 px-2 rounded-t mt-2">
+        <div className="bg-[#772BCB] w-full h-full pt-2 px-2 rounded-t-[10px] mt-2">
           <button
             onClick={() => dispatchArrowKey("ArrowUp")}
-            className={`bg-[#4D00A0] btn w-22 h-3 p-3 items-center flex justify-center rounded ring-neon ring-1 ring-offset-neon ring-offset-1 shadow-neon relative z-10 ${
+            className={`bg-[#4D00A0] btn w-22 h-3 p-3 items-center flex justify-center rounded-[5.36px] ring-neon ring-1 ring-offset-neon ring-offset-1 shadow-neon relative z-10 ${
               !playState && "animate-key-glow"
             }`}
           >
@@ -37,10 +37,10 @@ export default function ArrowKeys({
           </button>
         </div>
         <div className="w-full h-full"></div>
-        <div className="bg-[#772BCB] w-max h-full pl-2 py-2 rounded-l ml-2">
+        <div className="bg-[#772BCB] w-max h-full pl-2 py-2 rounded-l-[10px] ml-2">
           <button
             onClick={() => dispatchArrowKey("ArrowLeft")}
-            className={`bg-[#4D00A0] btn w-22 h-3 p-3 items-center flex justify-center rounded ring-neon ring-1 ring-offset-neon ring-offset-1 shadow-neon relative ${
+            className={`bg-[#4D00A0] btn w-22 h-3 p-3 items-center flex justify-center rounded-[5.36px] ring-neon ring-1 ring-offset-neon ring-offset-1 shadow-neon relative ${
               !playState && "animate-key-glow"
             }`}
           >
@@ -50,17 +50,17 @@ export default function ArrowKeys({
         <div className="bg-[#772BCB] w-full h-full p-2">
           <button
             onClick={() => dispatchArrowKey("ArrowDown")}
-            className={`bg-[#4D00A0] btn w-22 h-3 p-3 items-center flex justify-center rounded ring-neon ring-1 ring-offset-neon ring-offset-1 shadow-neon relative z-10 ${
+            className={`bg-[#4D00A0] btn w-22 h-3 p-3 items-center flex justify-center rounded-[5.36px] ring-neon ring-1 ring-offset-neon ring-offset-1 shadow-neon relative z-10 ${
               !playState && "animate-key-glow"
             }`}
           >
             <BiSolidDownArrow className="w-full h-full text-[#fff]" />
           </button>
         </div>
-        <div className="bg-[#772BCB] w-max h-full pr-2 py-2 rounded-r mr-2">
+        <div className="bg-[#772BCB] w-max h-full pr-2 py-2 rounded-r-[10px] mr-2">
           <button
             onClick={() => dispatchArrowKey("ArrowRight")}
-            className={`bg-[#4D00A0] btn w-22 h-3 p-3 items-center flex justify-center rounded ring-neon ring-1 ring-offset-neon ring-offset-1 shadow-neon relative ${
+            className={`bg-[#4D00A0] btn w-22 h-3 p-3 items-center flex justify-center rounded-[5.36px] ring-neon ring-1 ring-offset-neon ring-offset-1 shadow-neon relative ${
               !playState && "animate-key-glow"
             }`}
           >
