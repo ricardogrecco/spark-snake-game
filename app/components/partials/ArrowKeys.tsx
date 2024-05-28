337d034dce8aa5101e550199988562056354248e
Mark Roland Alueta <markintoshroland@gmail.com>
2024-05-28 08:49:35 +0800

    Update ArrowKeys styling

--- app/components/partials/ArrowKeys.tsx
+++ app/components/partials/ArrowKeys.tsx
@@ -29,7 +29,7 @@ export default function ArrowKeys({
         <div className="bg-[#772BCB] w-22 h-full px-2 rounded-t-[10px] mt-2 flex items-center justify-center">
           <button
             onClick={() => dispatchArrowKey("ArrowUp")}
-            className={`bg-[#4D00A0] btn w-22 h-3 items-center flex justify-center rounded-[5.36px] border-[#20FECB] hover:border-[#20FECB] border-[1px] shadow-neon relative z-10 ${
+            className={`bg-[#5A12AA] btn w-22 h-3 items-center flex justify-center rounded-[5.36px] border-[#FFFFFF] hover:border-[#FFFFFF] border-[1px] shadow-white-glow relative z-10 ${
               !playState && "animate-key-glow"
             }`}
           >
@@ -40,7 +40,7 @@ export default function ArrowKeys({
         <div className="bg-[#772BCB] w-22 h-full pl-2 py-2 rounded-l-[10px] ml-2 flex items-center justify-center">
           <button
             onClick={() => dispatchArrowKey("ArrowLeft")}
-            className={`bg-[#4D00A0] btn w-22 h-3 items-center flex justify-center rounded-[5.36px] border-[#20FECB] hover:border-[#20FECB] border-[1px] shadow-neon relative ${
+            className={`bg-[#5A12AA] btn w-22 h-3 items-center flex justify-center rounded-[5.36px] border-[#FFFFFF] hover:border-[#FFFFFF] border-[1px] shadow-white-glow relative ${
               !playState && "animate-key-glow"
             }`}
           >
@@ -50,7 +50,7 @@ export default function ArrowKeys({
         <div className="bg-[#772BCB] w-22 h-full p-2 flex items-center justify-center">
           <button
             onClick={() => dispatchArrowKey("ArrowDown")}
-            className={`bg-[#4D00A0] btn w-22 h-3 items-center flex justify-center rounded-[5.36px] border-[#20FECB] hover:border-[#20FECB] border-[1px] shadow-neon relative z-10 ${
+            className={`bg-[#5A12AA] btn w-22 h-3 items-center flex justify-center rounded-[5.36px] border-[#FFFFFF] hover:border-[#FFFFFF] border-[1px] shadow-white-glow relative z-10 ${
               !playState && "animate-key-glow"
             }`}
           >
@@ -60,7 +60,7 @@ export default function ArrowKeys({
         <div className="bg-[#772BCB] w-22 h-full pr-2 py-2 rounded-r-[10px] mr-2 flex items-center justify-center">
           <button
             onClick={() => dispatchArrowKey("ArrowRight")}
-            className={`bg-[#4D00A0] btn w-22 h-3 items-center flex justify-center rounded-[5.36px] border-[#20FECB] hover:border-[#20FECB] border-[1px] shadow-neon relative ${
+            className={`bg-[#5A12AA] btn w-22 h-3 items-center flex justify-center rounded-[5.36px] border-[#FFFFFF] hover:border-[#FFFFFF] border-[1px] shadow-white-glow relative ${
               !playState && "animate-key-glow"
             }`}
           >
