056ba377e65c82d03ab5531cf116b82bd351dee8
Mark Roland Alueta <markintoshroland@gmail.com>
2024-06-17 20:06:33 +0800

    Update Footer and components using it

--- app/components/partials/Footer.tsx
+++ app/components/partials/Footer.tsx
@@ -4,7 +4,7 @@ type FooterProps = HTMLAttributes<HTMLElement>;
 
 export default function Footer({ ...props }: FooterProps) {
   return (
-    <footer className="w-full font-medium mt-5 lg:mt-0" {...props}>
+    <footer className="w-full font-medium" {...props}>
       <a href="https://www.spark.co.nz/help/other/terms/competitions/spark-arcade">
         Terms and Conditions
       </a>
