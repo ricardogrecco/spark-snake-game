85be427383085aebabafb7f621ac59a2cd5f7979
Mark Roland Alueta <markintoshroland@gmail.com>
2024-06-17 19:58:19 +0800

    Refactor Footer

--- app/components/partials/Footer.tsx
+++ app/components/partials/Footer.tsx
@@ -4,33 +4,10 @@ type FooterProps = HTMLAttributes<HTMLElement>;
 
 export default function Footer({ ...props }: FooterProps) {
   return (
-    <footer
-      className="w-full font-medium text-xs md:text-sm mt-5 lg:mt-0"
-      {...props}
-    >
-      <ul className="flex flex-row items-center w-full justify-center text-center">
-        <li className="footer-item">
-          <a
-            href="https://www.spark.co.nz/help/privacy-and-safety/how-we-manage-privacy/spark-privacy-policies"
-            target="_blank"
-          >
-            Privacy
-          </a>
-        </li>
-        <li className="footer-item">
-          <a
-            href="https://www.spark.co.nz/help/other/legaldisclaimer/"
-            target="_blank"
-          >
-            Legal Disclaimer
-          </a>
-        </li>
-        <li className="footer-item">
-          <a href="https://www.spark.co.nz/help/other/terms">
-            Terms and Conditions
-          </a>
-        </li>
-      </ul>
+    <footer className="w-full font-medium mt-5 lg:mt-0" {...props}>
+      <a href="https://www.spark.co.nz/help/other/terms/competitions/spark-arcade">
+        Terms and Conditions
+      </a>
     </footer>
   );
 }
