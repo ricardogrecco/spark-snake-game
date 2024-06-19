e794cd33a819674332c109b17c3cc5f681692d26
Mark Roland Alueta <markintoshroland@gmail.com>
2024-06-19 10:10:19 +0800

    chore: Update metadataBase URL in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+const baseUrl =
+  process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL ||
+  "https://www.sparkarcade.co.nz/";
+
 export const metadata: Metadata = {
   title: "Spark Arcade - Spark NZ",
   description:
     "Dive into the exciting world of Spark Arcade and play your way to winning Spark Gift Cards.",
   applicationName: "Spark Arcade",
   keywords: ["Spark", "Arcade", "Games", "Rewards", "Spark NZ"],
-  metadataBase: new URL("https://www.sparkarcade.co.nz/"),
+  // metadataBase: new URL(baseUrl),
 };
 
 export default function RootLayout({
