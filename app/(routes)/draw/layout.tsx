c65189725bf78d969f13d52654be5f9fa7effc3d
Mark Roland Alueta <markintoshroland@gmail.com>
2024-07-11 22:04:03 +0800

    WIP: Draw page

---
+++ app/(routes)/draw/layout.tsx
@@ -0,0 +1,16 @@
+import MobileLayout from "@/app/components/layout/MobileLayout";
+import Drawer from "@/app/components/partials/Drawer";
+
+export default function DrawPageLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <Drawer>
+      <div className="flex flex-col items-center justify-center">
+        <MobileLayout>{children}</MobileLayout>
+      </div>
+    </Drawer>
+  );
+}
