3e96e8bdfc9a69191928051ce0655866f694fdc6
Mark Roland Alueta <markintoshroland@gmail.com>
2024-07-22 17:51:20 +0800

    Revert "WIP: Update page styling and content on Draw page"
    This reverts commit ab4bf8647384ecad262d9ae1ad02a608def1e25c.

--- app/(routes)/draw/page.tsx
+++ app/(routes)/draw/page.tsx
@@ -1,23 +1,3 @@
-import Footer from "@/app/components/partials/Footer";
-import SparkLogo from "@/public/assets/logo-spark2.svg";
-import { GiHamburgerMenu } from "react-icons/gi";
-
 export default function DrawPage() {
-  return (
-    <main className="flex min-h-screen w-full flex-col items-center p-8">
-      <div className="flex w-full flex-row items-center justify-between">
-        <SparkLogo className="w-32 self-center md:w-52" />
-        <label
-          htmlFor="side-drawer"
-          className="btn btn-ghost drawer-button -m-5"
-        >
-          <GiHamburgerMenu className="h-6 w-6" />
-        </label>
-      </div>
-      <div className="mt-5 flex max-w-sm flex-grow flex-col text-center font-semibold leading-[24px]">
-        <div className="flex-grow"></div>
-        <Footer />
-      </div>
-    </main>
-  );
+  return <main>DrawPage</main>;
 }
